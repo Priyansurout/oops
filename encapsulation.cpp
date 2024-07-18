4582f6de63a8eed5c9be5841a701d3c067a52670
Priyansu Rout <priyansurout192004@gmail.com>
2024-07-18 10:26:24 +0530

    OOPS - Encapsulation/Abstraction

---
+++ encapsulation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Encapsulation - wrapping up of data and information in a single unit, while controlling access to them. [varable | function]
+// jo bhai tuma varible and methos hai usko class mai lik do
+
+// Abstraction - Display only essential information and hiding the details
+
+int main()
+{
+    return 0;
+}
